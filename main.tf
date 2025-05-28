85647cfe25abcbb660b8d66d41ac77a08342de39
shajahan <shajahan@test.com>
2025-05-28 12:06:58 +0530

    2nd commit completed

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_vpc" "eusvpc" {
     cidr_block = "10.133.0.0/16"
     tags = {
       Name = "eu-svpc"
-      Environment = "eu-svpc"
-      git = "v2"
+      Environment = "eu-svpcgit"
+      git = "v2233"
     }
   
 }
